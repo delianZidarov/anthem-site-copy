36fe3331c5705459a926ef34bf0e594cf4aa1d34
Delian Zidarov <d.zidarov@gmail.com>
2022-06-09 17:52:26 +0900

    finished the footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navigation from "./components/Navigation.js";
+import Footer from "./components/Footer.js";
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import "./css/Footer.css";
+function Footer() {
+  return (
+    <footer className="container-fluid fixed-bottom text-center site-footer">
+      <div className="row">
+        <div className="col-12">
+          <div className="social-links">
+            <a href="#" className="btn">
+              <FontAwesomeIcon icon={faInstagram} className="fontawesome-icon" />
+            </a>
+            <a href="#" className="btn">
+              <FontAwesomeIcon icon={faFacebookF} className="fontawesome-icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="row pt-3">
+        <div className="col-12 mb-2">
+          ALL RIGHTS RESERVED <br /> ANTHEM COFFEE AND TEA
+        </div>
+      </div>
+
+      <div className="row pb-3 mb-2">
+        <div className="col-12">COPYRIGHT 2019</div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
