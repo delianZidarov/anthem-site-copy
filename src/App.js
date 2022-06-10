2e8b5c08f7814998f454616f0edbde6e71edb037
Delian Zidarov <d.zidarov@gmail.com>
2022-06-10 19:32:03 +0900

    some css adjustments

--- src/App.js
+++ src/App.js
@@ -69,55 +69,69 @@ function App() {
   return (
     <div className="App">
       <main>
-        <div className="container">
-          <div className="row gx-0">
-            <div className="col-2">
-              <div className="car-image prev"></div>
+        <div className="container-fluid main-display p-0">
+          <div className="row ms-5 me-5">
+            <div className="col-12 col-sm-8 offset-sm-2 p-0 pt-5 col-xl-6 offset-xl-3">
+              <a href="#" className="btn giftcard-btn p-md-3 pd-lg-5 w-100 ms-sm-2 me-sm-2 ms-lg-5 me-lg-5 mt-5 mb-4">
+                CHECK YOUR GIFT CARD BALANCE - OR - PURCHASE EGIFT CARDS
+              </a>
             </div>
-            <div className="col-8">
-              <div className="carousel slide" data-bs-ride="carousel" id="productCarousel">
-                <div className="carousel-inner">
-                  <div className="carousel-item active">
-                    <img src="/images/lightbox-cookies.jpg" alt="cookies" className="d-block " />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="/images/lightbox-energy.jpg" alt="energy drinks" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-late-art.jpg" alt="latte art" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-lemonloaf.jpg" alt="lemon loaf" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-sandwhich.jpg" alt="sandwhich" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-shakarato.jpg" alt="shakarato" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-shakes.jpg" alt="shakes" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-tea.jpg" alt="tea for 3" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-tea-closeup.jpg" alt="tea" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-tehetian.jpg" alt="tehetian" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-vibes.jpg" alt="vibes" className="d-block " />
-                  </div>
-                  <div className="carousel-item ">
-                    <img src="/images/lightbox-yum.jpg" alt="yum" className="d-block " />
+          </div>
+          <div>
+            <div className="row carousel-row gx-0 ms-5 me-5 ps-2 pe-2">
+              <div className="col-2 col-xl-2 offset-xl-1">
+                <div className="car-image prev"></div>
+              </div>
+              <div className="col-8 col-xl-6">
+                <div className="carousel slide" data-bs-ride="carousel" id="productCarousel">
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                      <img src="/images/lightbox-cookies.jpg" alt="cookies" className="d-block " />
+                    </div>
+                    <div className="carousel-item">
+                      <img src="/images/lightbox-energy.jpg" alt="energy drinks" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-late-art.jpg" alt="latte art" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-lemonloaf.jpg" alt="lemon loaf" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-sandwhich.jpg" alt="sandwhich" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-shakarato.jpg" alt="shakarato" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-shakes.jpg" alt="shakes" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-tea.jpg" alt="tea for 3" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-tea-closeup.jpg" alt="tea" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-tehetian.jpg" alt="tehetian" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-vibes.jpg" alt="vibes" className="d-block " />
+                    </div>
+                    <div className="carousel-item ">
+                      <img src="/images/lightbox-yum.jpg" alt="yum" className="d-block " />
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className="col-2 col-xl-2">
+                <div className="car-image next"></div>
+              </div>
             </div>
-            <div className="col-2">
-              <div className="car-image next"></div>
+          </div>
+          <div className="row">
+            <div className="col-8 offset-2 text-center">
+              <h1>EXPERIENCE HEROIC HOSPITALITY</h1>
             </div>
           </div>
         </div>
